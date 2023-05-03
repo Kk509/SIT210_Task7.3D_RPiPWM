c8b833203ff49962866876fe5c7c3baf44aec74e
Kk509 <113608904+Kk509@users.noreply.github.com>
2023-05-03 14:32:15 +1000

    Update pwm.py

--- pwm.py
+++ pwm.py
@@ -1,30 +1,63 @@
 import RPi.GPIO as GPIO
 import time
-ledpin = 17
+import numpy as np
+
+#GPIO pins
+led = 17
+trig = 4
+echo = 27
+
+#set GPIO mode
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(ledpin, GPIO.OUT)
 
-#create pwm instance with frequency 1000Hz
-led_pwm = GPIO.PWM(ledpin, 1000)
-#start the pwm at 0 duty cycle
-led_pwm.start(0)
+#setup
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(trig, GPIO.OUT)
+GPIO.setup(echo, GPIO.IN)
 
-try:    
-	while True:
-		#for loop from 0-100% duty 
-		for duty in range(0, 101):
-			led_pwm.ChangeDutyCycle(duty)
-			#generate delay every 10 millisecond
-			time.sleep(0.01)
+#distance and duty range
+distance_range = np.array([10, 20, 50, 100])
+duty_cycle_range = np.array([100, 50, 25, 0])
 
-		time.sleep(0.5)
+#create pwm instance, start pwm at 0 duty cycle
+pwm = GPIO.PWM(led, 100)
+pwm.start(0)
 
-		#for loop from 100-0% duty
-		for duty in range(100, -1, -1):
-			led_pwm.ChangeDutyCycle(duty)
-			time.sleep(0.01)
+def distance():
 
-		time.sleep(0.5)
+        GPIO.output(trig, True)
+        time.sleep(0.00001)
+        GPIO.output(trig, False)
 
+        while GPIO.input(echo)==0:
+                pulse_start = time.time()
+        while GPIO.input(echo)==1:
+                pulse_end = time.time()
+
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17150
+        distance = round(distance, 2)
+
+        return distance
+
+try:
+        GPIO.output(trig, GPIO.LOW)
+        print ("Waiting for sensor to settle")
+        time.sleep(2)
+
+        current_duty_cycle = 0
+        while True:
+                x = distance()
+
+                duty_cycle = np.interp(x, distance_range, duty_cycle_range)
+                step = 1 if duty_cycle > current_duty_cycle else -1
+                for i in range(int(current_duty_cycle), int(duty_cycle), step):
+                        pwm.ChangeDutyCycle(i)
+                        time.sleep(0.01) 
+                
+		current_duty_cycle = duty_cycle
+                print ("distance:", x, "cm")
+                time.sleep(0.5)
+		
 except KeyboardInterrupt:
         GPIO.cleanup()
